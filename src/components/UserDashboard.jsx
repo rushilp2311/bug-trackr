e03da39083875ae2b6d3da07d3643fc87eb0fa4e
Rushil Patel <rushilp2311@gmail.com>
2020-10-23 19:40:35 -0400

    Open and Close Bugs List Separated

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -9,49 +9,59 @@ import { Link } from "react-router-dom";
 
 function UserDashboard() {
   const teamContext = useContext(TeamContext);
+  let openBugsCount = 0;
+  let closeBugsCount = 0;
+  if (teamContext.currentTeam != null) {
+    openBugsCount = teamContext.currentTeam.bugs.filter(
+      (bug) => bug.isOpen === true
+    ).length;
+    closeBugsCount = teamContext.currentTeam.bugs.filter(
+      (bug) => bug.isOpen !== true
+    ).length;
+  }
   return (
     <>
       {teamContext.currentTeam ? (
-        <div className='dashboard__container'>
-          <div className='dashboard__header'>
-            <div className='team_name'>
+        <div className="dashboard__container">
+          <div className="dashboard__header">
+            <div className="team_name">
               <h2>
-                <Link to='/teamdetails' style={{ color: "#001233" }}>
+                <Link to="/teamdetails" style={{ color: "#001233" }}>
                   {teamContext.currentTeam.name}
                 </Link>
               </h2>
             </div>
-            <div className='dashboard__controls'>
+            <div className="dashboard__controls">
               <input
-                type='text'
-                name='search'
-                id='bug_search'
-                aria-label='Search all bugs'
-                placeholder='Search all bugs'
+                type="text"
+                name="search"
+                id="bug_search"
+                aria-label="Search all bugs"
+                placeholder="Search all bugs"
               />
-              <FcSearch className='search_logo' />
+              <FcSearch className="search_logo" />
               <AddBug />
             </div>
           </div>
-          <div className='dashboard__body'>
-            <div className='dashboard__info'>
-              <div className='dashboard__bugstatus'>
-                <div className='open__bugs'>
+          <div className="dashboard__body">
+            <div className="dashboard__info">
+              <div className="dashboard__bugstatus">
+                <div className="open__bugs">
                   <span>
                     <GoIssueOpened />
                   </span>{" "}
-                  10 Open
+                  {openBugsCount} Open
                 </div>
-                <div className='closed__bugs'>
+                <div className="closed__bugs">
                   <span>
                     <FcCheckmark />
                   </span>{" "}
-                  11 Closed
+                  {closeBugsCount} Closed
                 </div>
               </div>
-              <div className='dashboard__filters'>
+              <div className="dashboard__filters">
                 <details>
-                  <summary role='button'>
+                  <summary role="button">
                     Author
                     <span>
                       <RiArrowDropDownFill />
@@ -68,7 +78,7 @@ function UserDashboard() {
                   </details-menu>
                 </details>
                 <details>
-                  <summary role='button'>
+                  <summary role="button">
                     Sort
                     <span>
                       <RiArrowDropDownFill />{" "}
@@ -84,40 +94,43 @@ function UserDashboard() {
                 </details>
               </div>
             </div>
-            <div className='dashboard__bugslist'>
+            <div className="dashboard__bugslist">
               <ul>
-                {teamContext.currentTeam.bugs.map(bug => (
-                  <li className='bugs__list' key={bug._id}>
-                    <div className='bugs__list__content'>
-                      <div className='bugs__list__info__header'>
-                        <span>
-                          <GoIssueOpened />
-                        </span>
-                        <div className='bugs__list__headers'>
-                          <p className='header'>
-                            <Link
-                              to={{ pathname: "/bugdetails", state: bug }}
-                              style={{ color: "#001233" }}>
-                              {bug.title}
-                            </Link>
-                          </p>
-                          <p className='subheader'>
-                            Opened on {bug.date.slice(0, 10)} by{" "}
-                            {bug.createdBy.name}
-                          </p>
+                {teamContext.currentTeam.bugs.map((bug) =>
+                  bug.isOpen ? (
+                    <li className="bugs__list" key={bug._id}>
+                      <div className="bugs__list__content">
+                        <div className="bugs__list__info__header">
+                          <span>
+                            <GoIssueOpened />
+                          </span>
+                          <div className="bugs__list__headers">
+                            <p className="header">
+                              <Link
+                                to={{ pathname: "/bugdetails", state: bug }}
+                                style={{ color: "#001233" }}
+                              >
+                                {bug.title}
+                              </Link>
+                            </p>
+                            <p className="subheader">
+                              Opened on {bug.date.slice(0, 10)} by{" "}
+                              {bug.createdBy.name}
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className='bugs__list__comments'>
-                      <p>
-                        <span>
-                          <GoComment />
-                        </span>{" "}
-                        {bug.comments.length}
-                      </p>
-                    </div>
-                  </li>
-                ))}
+                      <div className="bugs__list__comments">
+                        <p>
+                          <span>
+                            <GoComment />
+                          </span>{" "}
+                          {bug.comments.length}
+                        </p>
+                      </div>
+                    </li>
+                  ) : null
+                )}
               </ul>
             </div>
           </div>
